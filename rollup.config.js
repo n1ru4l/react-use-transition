241dd93dfce99612c5aad4bb8295d667116cf91d
n1ru4l <laurinquast@googlemail.com>
2021-04-13 09:28:37 +0200

    chore: tests + reformat code

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,39 @@
-import ts from '@rollup/plugin-typescript'
-import { promises as fs } from "fs"
+import ts from "@rollup/plugin-typescript";
+import { promises as fs } from "fs";
 
-const isCJSBuild = process.env.MODE === "cjs" 
+const isCJSBuild = process.env.MODE === "cjs";
 
 const commonjsPkgJSONPlugin = () => {
   return {
-    name: 'commonjsPkgJSONPlugin',
-    writeBundle: async  () => {
+    name: "commonjsPkgJSONPlugin",
+    writeBundle: async () => {
       if (isCJSBuild === true) {
-        await fs.writeFile("dist/cjs/package.json", JSON.stringify({
-          "type": "commonjs"
-        }))
+        await fs.writeFile(
+          "dist/cjs/package.json",
+          JSON.stringify({
+            type: "commonjs",
+          })
+        );
       } else {
-        await fs.copyFile("package.json", "dist/package.json")
+        await fs.copyFile("package.json", "dist/package.json");
       }
-    }
-  }
-}
+    },
+  };
+};
 
 export default {
-  input: ['src/main.ts'],
+  input: ["src/main.ts"],
   output: [
     {
-      dir: isCJSBuild ? 'dist/cjs' : 'dist',
-      format: isCJSBuild ? 'cjs' : 'esm'
-    }
+      dir: isCJSBuild ? "dist/cjs" : "dist",
+      format: isCJSBuild ? "cjs" : "esm",
+    },
   ],
-  plugins: [ts({ tsconfig: isCJSBuild ? "tsconfig.build-cjs.json" : "tsconfig.build.json" }), commonjsPkgJSONPlugin()],
-  external: ["react"]
-}
+  plugins: [
+    ts({
+      tsconfig: isCJSBuild ? "tsconfig.build-cjs.json" : "tsconfig.build.json",
+    }),
+    commonjsPkgJSONPlugin(),
+  ],
+  external: ["react"],
+};
